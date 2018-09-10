570e5f95143325ea62196ae5380defe05cd2c80f
gliso <giusliso@gmail.com>
2018-09-10 19:09:49 +0200

    Integration of Material, Fix Packages references

--- src/app/SharedModule/shared.module.ts
+++ src/app/SharedModule/shared.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RootStoreModule } from '../root-store';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule, MatCardModule} from '@angular/material';
 @NgModule({
   imports: [
     CommonModule,
-    RootStoreModule
+    RootStoreModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatCardModule
   ],
   declarations: [],
   exports:[
-    RootStoreModule
+    RootStoreModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatCardModule
   ]
 })
 export class SharedModule {}
